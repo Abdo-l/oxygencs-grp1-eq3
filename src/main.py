ac08835e5b512d5f4f0ce9b49332c7afd9b1d0ab
FaridCompaore <farid-abdoulaye.compaore.1@etsmtl.net>
2024-06-29 17:46:42 -0400

    preGitCommit test reussi

--- src/main.py
+++ src/main.py
@@ -1,39 +1,81 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
+"""
+Module principal pour l'application Oxygen CS.
+
+Ce module contient la classe principale App qui gère la connexion au hub de capteurs,
+la gestion des données de capteurs et les interactions avec un service HVAC.
+"""
+
+import os
 import logging
-import requests
 import json
 import time
 
+import psycopg2
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+import requests
+
 
 class App:
-    def __init__(self):
-        self._hub_connection = None
-        self.TICKS = 10
+    """
+    Classe principale de l'application Oxygen CS.
 
-        # To be configured by your team
-        self.HOST = None  # Setup your host here
-        self.TOKEN = None  # Setup your token here
-        self.T_MAX = None  # Setup your max temperature here
-        self.T_MIN = None  # Setup your min temperature here
-        self.DATABASE_URL = None  # Setup your database here
+    Attributes:
+        host (str): Hôte pour la configuration.
+        token (str): Jeton pour l'authentification.
+        t_max (str): Température maximale configurée.
+        t_min (str): Température minimale configurée.
+        connection: Connexion à la base de données PostgreSQL.
+        _hub_connection: Connexion au hub de capteurs.
+    """
+
+    def __init__(self):
+        """
+        Initialise l'application avec la configuration et la connexion à la base de données.
+        """
+        self._hub_connection = None
+        self.ticks = 10
+
+        # À configurer par votre équipe
+        self.host = os.getenv("HOST")  # Configurez votre hôte ici
+        self.token = os.getenv("TOKEN")  # Configurez votre jeton ici
+        self.t_max = os.getenv("T_MAX")  # Configurez votre température maximale ici
+        self.t_min = os.getenv("T_MIN")  # Configurez votre température minimale ici
+
+        try:
+            self.connection = psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                port=os.getenv("DB_PORT"),
+            )
+        except psycopg2.Error as e:
+            print("Erreur de connexion à la base de données : ", e)
 
     def __del__(self):
-        if self._hub_connection != None:
+        """
+        Arrête la connexion au hub lorsque l'application est supprimée.
+        """
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def start(self):
-        """Start Oxygen CS."""
+        """
+        Lance l'application Oxygen CS.
+        """
         self.setup_sensor_hub()
         self._hub_connection.start()
-        print("Press CTRL+C to exit.")
+        print("Appuyez sur CTRL+C pour quitter.")
         while True:
             time.sleep(2)
 
     def setup_sensor_hub(self):
-        """Configure hub connection and subscribe to sensor data events."""
+        """
+        Configure la connexion au hub de capteurs et s'abonne aux événements de données de capteurs.
+        """
         self._hub_connection = (
             HubConnectionBuilder()
-            .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")
+            .with_url(f"{self.host}/SensorHub?token={self.token}")
             .configure_logging(logging.INFO)
             .with_automatic_reconnect(
                 {
@@ -46,44 +88,65 @@ class App:
             .build()
         )
         self._hub_connection.on("ReceiveSensorData", self.on_sensor_data_received)
-        self._hub_connection.on_open(lambda: print("||| Connection opened."))
-        self._hub_connection.on_close(lambda: print("||| Connection closed."))
+        self._hub_connection.on_open(lambda: print("||| Connexion ouverte."))
+        self._hub_connection.on_close(lambda: print("||| Connexion fermée."))
         self._hub_connection.on_error(
-            lambda data: print(f"||| An exception was thrown closed: {data.error}")
+            lambda data: print(f"||| Une exception a été levée : {data.error}")
         )
 
     def on_sensor_data_received(self, data):
-        """Callback method to handle sensor data on reception."""
+        """
+        Méthode de rappel pour gérer les données de capteurs reçues.
+        """
         try:
             print(data[0]["date"] + " --> " + data[0]["data"], flush=True)
             timestamp = data[0]["date"]
             temperature = float(data[0]["data"])
-            self.take_action(temperature)
-            self.save_event_to_database(timestamp, temperature)
-        except Exception as err:
+            etat = self.take_action(temperature)
+            self.save_event_to_database(timestamp, temperature, etat)
+        except Exception as err:  # pylint: disable=broad-except
             print(err)
 
     def take_action(self, temperature):
-        """Take action to HVAC depending on current temperature."""
-        if float(temperature) >= float(self.T_MAX):
-            self.send_action_to_hvac("TurnOnAc")
-        elif float(temperature) <= float(self.T_MIN):
-            self.send_action_to_hvac("TurnOnHeater")
+        """
+        Prend des mesures HVAC en fonction de la température actuelle.
+        """
+        if float(10) >= float(5):
+            return self.send_action_to_hvac("TurnOnAc")
+        if float(temperature) <= float(self.t_min):
+            return self.send_action_to_hvac("TurnOnHeater")
+        return None
 
     def send_action_to_hvac(self, action):
-        """Send action query to the HVAC service."""
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{self.TICKS}")
-        details = json.loads(r.text)
-        print(details, flush=True)
-
-    def save_event_to_database(self, timestamp, temperature):
-        """Save sensor data into database."""
+        """
+        Envoie une requête d'action au service HVAC.
+        """
         try:
-            # To implement
-            pass
-        except requests.exceptions.RequestException as e:
-            # To implement
-            pass
+            r = requests.get(
+                f"{self.host}/api/hvac/{self.token}/{action}/{self.ticks}", timeout=10
+            )
+            details = json.loads(r.text)
+            print(details, flush=True)
+            return details["Response"]
+        except requests.RequestException as e:
+            print(f"Erreur lors de l'envoi de l'action HVAC : {e}")
+            return None
+
+    def save_event_to_database(self, timestamp, temperature, etat):
+        """
+        Enregistre les données de capteurs dans la base de données.
+        """
+        try:
+            cur = self.connection.cursor()
+            cur.execute(
+                "INSERT INTO sensor (temperature, heure, etat)"
+                + "VALUES (%s, %s, %s) ON CONFLICT (id) DO NOTHING",
+                (temperature, timestamp, etat),
+            )
+            self.connection.commit()
+            cur.close()
+        except psycopg2.Error as e:
+            print("Erreur lors de l'enregistrement dans la base de données : ", e)
 
 
 if __name__ == "__main__":
---
+++ test/test_unitaire.py
@@ -0,0 +1,27 @@
+## To Implement
+"""
+Module de tests pour OxygenCS.
+
+Ce module contient des tests unitaires pour 
+vérifier les fonctionnalités de base de l'application OxygenCS.
+"""
+
+
+def test_addition():
+    """Test the addition of two numbers."""
+    assert 1 + 1 == 2
+
+
+def test_subtraction():
+    """Test the subtraction of two numbers."""
+    assert 5 - 3 == 2
+
+
+def test_addition_large():
+    """Test the addition of two larger numbers."""
+    assert 11 + 1 == 12
+
+
+def test_addition_larger():
+    """Test the addition of two even larger numbers."""
+    assert 11 + 11 == 222
