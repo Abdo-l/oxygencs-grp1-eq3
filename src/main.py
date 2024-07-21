e41231c6dd2498f8f0eeb6ded7d5abd14340fb5c
FaridCompaore <faridfreestyle11@outlook.com>
2024-07-21 00:18:06 -0400

    clean  code

--- src/main.py
+++ src/main.py
@@ -117,7 +117,7 @@ class App:
         if float(temperature) >= float(self.t_max):
             return self.send_action_to_hvac("TurnOnAc")
         if float(temperature) <= float(self.t_min):
-            return self.send_action_to_hvac("TurnOnHeater") + self.t_min
+            return self.send_action_to_hvac("TurnOnHeater")
         return None
 
     def send_action_to_hvac(self, action):
